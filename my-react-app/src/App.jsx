35eb23ff9c98aaff6b64a5ffc7e09294da930fb2
prathamesh-pal <prathameshpal86@gmail.com>
2025-07-09 14:33:52 +0530

    list rendaring

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,22 @@
-import Usergreeting from "./Usergreeting.jsx"
-
+import List from "./list.jsx"
 function App() {
+  const fruits = [{ name: "apple", calories: 10 },
+  { name: "orange", calories: 32 },
+  { name: "papaya", calories: 54 },
+  { name: "graps", calories: 24 },
+  { name: "tamato", calories: 122 }]
+
+  const vagitable = [{ name: "apple", calories: 10 },
+  { name: "orange", calories: 32 },
+  { name: "papaya", calories: 54 },
+  { name: "graps", calories: 24 },
+  { name: "tamato", calories: 122 }]
+
   return (
     <>
+      < List items={fruits} category="Fruits" ></List>
+      < List items={vagitable} category="vagitables" ></List>
+
     </>
   )
 }
