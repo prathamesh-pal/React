f8af020a242b7cbdc6274e37ff52f7746c7c800f
prathamesh-pal <prathameshpal86@gmail.com>
2026-07-05 13:11:19 +0530

    change

--- Day7/chater/src/App.jsx
+++ Day7/chater/src/App.jsx
@@ -6,7 +6,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "animate.css";
 
-// const API_KEY = "";
  
 function App() {
   const [message, setMessage] = useState("");
